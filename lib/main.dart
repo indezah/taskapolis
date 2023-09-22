a59e699911a7664958b46b943b8777a463d97455
Nisura Indisa <nisuraindisa21@gmail.com>
2023-09-23 03:44:02 +0530

    Added remembering login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:taskapolis/firebase_options.dart';
+import 'package:taskapolis/pages/home.dart';
 
 import 'package:taskapolis/pages/signin.dart';
 
@@ -13,13 +17,42 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  late StreamSubscription<User?> user;
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    user.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? '/signin' : '/home',
+      routes: {
+        '/signin': (context) => SigninScreen(),
+        '/home': (context) => HomePage(),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily: 'Inter',
