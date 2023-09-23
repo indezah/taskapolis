c3d2ea767037f57e66d8f3ae8510fede62d9424f
Nisura Indisa <nisuraindisa21@gmail.com>
2023-09-23 23:11:35 +0530

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,6 +14,8 @@ List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   String selectedCategory = 'Personal';
   String selectedFilter = 'All';
+  final String userId =
+      FirebaseAuth.instance.currentUser!.uid; // Get the current user's ID
 
   void onCategorySelected(String category) {
     setState(() {
@@ -31,91 +33,131 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Taskapolis"),
+        title: Text('Hello'),
       ),
-      body: Column(
-        children: <Widget>[
-          DropdownButton<String>(
-            value: selectedCategory,
-            items: <String>['Personal', 'Work', 'Sports'].map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (String? category) {
-              onCategorySelected(category!);
-            },
-          ),
-          Row(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
             children: <Widget>[
-              FilterChip(
-                label: Text('All'),
-                selected: selectedFilter == 'All',
-                onSelected: (isSelected) {
-                  onFilterSelected('All');
-                },
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1.0,
+                      blurRadius: 5.0,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(15.0),
+                  border: Border.all(
+                      color: Colors.blueAccent,
+                      style: BorderStyle.solid,
+                      width: 0.90),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    value: selectedCategory,
+                    items: <String>['Personal', 'Work', 'Sports']
+                        .map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      onCategorySelected(value!);
+                    },
+                  ),
+                ),
               ),
-              FilterChip(
-                label: Text('Pending'),
-                selected: selectedFilter == 'Pending',
-                onSelected: (isSelected) {
-                  onFilterSelected('Pending');
-                },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FilterChip(
+                      label: Text('All'),
+                      selected: selectedFilter == 'All',
+                      onSelected: (isSelected) {
+                        onFilterSelected('All');
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FilterChip(
+                      label: Text('Pending'),
+                      selected: selectedFilter == 'Pending',
+                      onSelected: (isSelected) {
+                        onFilterSelected('Pending');
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FilterChip(
+                      label: Text('Completed'),
+                      selected: selectedFilter == 'Completed',
+                      onSelected: (isSelected) {
+                        onFilterSelected('Completed');
+                      },
+                    ),
+                  ),
+                ],
               ),
-              FilterChip(
-                label: Text('Completed'),
-                selected: selectedFilter == 'Completed',
-                onSelected: (isSelected) {
-                  onFilterSelected('Completed');
-                },
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(userId)
+                      .collection('tasks')
+                      .where('category', isEqualTo: selectedCategory)
+                      // if selectedFilter is 'All', then don't filter
+                      .where('status',
+                          isEqualTo: selectedFilter == 'All'
+                              ? 'Pending'
+                              : selectedFilter)
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+
+                    if (snapshot.data!.docs.isEmpty) {
+                      return Center(child: Text('No tasks'));
+                    }
+
+                    List<Map<String, dynamic>> allTasks =
+                        snapshot.data!.docs.map((DocumentSnapshot doc) {
+                      Map<String, dynamic> data =
+                          doc.data() as Map<String, dynamic>;
+                      return data;
+                    }).toList();
+
+                    return ListView.builder(
+                        itemCount: allTasks.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(allTasks[index]['title']),
+                            subtitle: Text(allTasks[index]['category']),
+                          );
+                        });
+                  },
+                ),
               ),
             ],
           ),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(FirebaseAuth.instance.currentUser?.uid)
-                  .collection('tasks')
-                  .where('category', isEqualTo: selectedCategory)
-                  .where('status', isEqualTo: selectedFilter)
-                  .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Something went wrong');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  print(
-                    FirebaseAuth.instance.currentUser?.email,
-                  );
-
-                  print(selectedCategory);
-                  print(selectedFilter);
-                  return Text("Loading");
-                }
-
-                if (snapshot.data!.docs.isEmpty) {
-                  return Center(child: Text('No tasks'));
-                }
-
-                return ListView(
-                  children:
-                      snapshot.data!.docs.map((DocumentSnapshot document) {
-                    Map<String, dynamic> data =
-                        document.data() as Map<String, dynamic>;
-                    return ListTile(
-                      title: Text(data['title']),
-                      subtitle: Text(data['category']),
-                    );
-                  }).toList(),
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
