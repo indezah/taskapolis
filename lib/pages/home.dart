16f6e510ad247b0258cd2753fe262bb6e0bf7735
Nisura Indisa <nisuraindisa21@gmail.com>
2023-09-23 16:16:43 +0530

    works :D

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,178 +9,113 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  late final TabController _tabController;
+  String selectedCategory = 'Personal';
+  String selectedFilter = 'All';
 
-  final user = FirebaseAuth.instance.currentUser;
-
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+  void onCategorySelected(String category) {
+    setState(() {
+      selectedCategory = category;
+    });
   }
 
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
+  void onFilterSelected(String filter) {
+    setState(() {
+      selectedFilter = filter;
+    });
   }
-
-  var allTasks = [];
-
-  var pendingTasks = [];
-
-  var completedTasks = [];
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 3,
-      child: Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              ListTile(
-                title: Text('Home'),
-                onTap: () {
-                  // Navigate to the home page
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Taskapolis"),
+      ),
+      body: Column(
+        children: <Widget>[
+          DropdownButton<String>(
+            value: selectedCategory,
+            items: <String>['Personal', 'Work', 'Sports'].map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? category) {
+              onCategorySelected(category!);
+            },
+          ),
+          Row(
+            children: <Widget>[
+              FilterChip(
+                label: Text('All'),
+                selected: selectedFilter == 'All',
+                onSelected: (isSelected) {
+                  onFilterSelected('All');
+                },
+              ),
+              FilterChip(
+                label: Text('Pending'),
+                selected: selectedFilter == 'Pending',
+                onSelected: (isSelected) {
+                  onFilterSelected('Pending');
+                },
+              ),
+              FilterChip(
+                label: Text('Completed'),
+                selected: selectedFilter == 'Completed',
+                onSelected: (isSelected) {
+                  onFilterSelected('Completed');
+                },
+              ),
+            ],
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(FirebaseAuth.instance.currentUser?.uid)
+                  .collection('tasks')
+                  .where('category', isEqualTo: selectedCategory)
+                  .where('status', isEqualTo: selectedFilter)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  print(
+                    FirebaseAuth.instance.currentUser?.email,
                   );
-                },
-              ),
-              ListTile(
-                title: Text('Logout'),
-                onTap: () {
-                  // Navigate to the settings page
-                  FirebaseAuth.instance.signOut().then((value) =>
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => AuthPage())));
-                },
-              ),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          // scrolledUnderElevation: scrolledUnderElevation,
-          title: Center(
-              child: Text(
-            'TASKAPOLIS',
-            style: TextStyle(
-              fontFamily: 'Space-Grotesk',
-              fontWeight: FontWeight.bold, // Apply bold style
-            ),
-          )),
-          bottom: TabBar(
-            controller: _tabController,
-            tabs: const <Widget>[
-              Tab(
-                text: 'ALL',
-              ),
-              Tab(
-                text: 'PENDING',
-              ),
-              Tab(
-                text: 'COMPLETED',
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                // // Implement search feature
-                // showSearch(context: context, delegate: TaskSearch(allTasks));
-              },
-            ),
-          ],
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            RefreshIndicator(
-              onRefresh: () async {
-                // Reload the data here
-                // You can call your function to fetch the data here
-                // For example, fetchTodoItems();
-              },
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(user?.uid)
-                    .collection('tasks')
-                    .snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Something went wrong');
-                  }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Container(
-                        alignment: Alignment.center,
-                        child: CircularProgressIndicator());
-                  }
+                  print(selectedCategory);
+                  print(selectedFilter);
+                  return Text("Loading");
+                }
 
-                  allTasks = snapshot.data!.docs.map((DocumentSnapshot doc) {
+                if (snapshot.data!.docs.isEmpty) {
+                  return Center(child: Text('No tasks'));
+                }
+
+                return ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
                     Map<String, dynamic> data =
-                        doc.data()! as Map<String, dynamic>;
-                    return data;
-                  }).toList();
-
-                  print(allTasks);
-
-                  return ListView.builder(
-                    itemCount: allTasks.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: ListTile(
-                          title: Text("${allTasks[index]['name']}",
-                              style: const TextStyle(
-                                  fontFamily: 'Inter',
-                                  fontWeight: FontWeight.w600)),
-                          leading: FlutterLogo(size: 56.0),
-                          subtitle: Text('Here is a second line'),
-                          trailing: Checkbox(
-                            onChanged: (value) => {
-                              // setState(() {
-                              //   allTasks[index].isDone = value!;
-                              // })
-                            },
-                            value: false,
-                          ),
-                          onTap: () {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) =>
-                          },
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
+                        document.data() as Map<String, dynamic>;
+                    return ListTile(
+                      title: Text(data['title']),
+                      subtitle: Text(data['category']),
+                    );
+                  }).toList(),
+                );
+              },
             ),
-            Center(
-              child: Text("It's rainy here"),
-            ),
-            Center(
-              child: Text("It's sunny here"),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            // // Navigate to the task input screen
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => AddTaskPage()),
-            // );
-          },
-        ),
+          ),
+        ],
       ),
     );
   }
